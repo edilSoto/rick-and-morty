b32e5bd3cd92ff7528936e6893a957771f5a9045
Edil <esh9912@gmail.com>
2025-07-07 11:41:54 -0400

    Working on main layout and basic navbar

---
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [NavbarComponent],
+  templateUrl: './main-layout.component.html'
+})
+export default class MainLayoutComponent {
+
+}
